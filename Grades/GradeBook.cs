15407b1e14a0faf946de2b2d85cca207092afddd
Dev <Dev@DESKTOP-0DNU43G>
2016-11-08 22:19:40 -0600

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+   
+    class GradeBook
+    {
+       private List<float> grades = new List<float>();
+
+        public GradeBook()
+        {
+
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+            
+        }
+
+       
+
+    }
+}
